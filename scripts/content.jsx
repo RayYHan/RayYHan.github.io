bc1780ccb2ddb0616fb7c9963e1c1eca2d28c624
Ray Y Han <giraiday@gmail.com>
2016-10-17 22:29:36 -0700

    navbar fixed to top

--- scripts/content.jsx
+++ scripts/content.jsx
@@ -12,58 +12,36 @@ var education = {
   description: "I have graduated with the honor from California State University, Los Angeles with a Bachelor of Science in computer science. During my undergraduate years, I have excelled in all courses, received several honors and rewards and participated in many projects and events."
 };
 
+var projects = {
+  title: "Project",
+  description: "I have worked on several projects for both academic and professional purposes"
+}
+
 //Color Scheme used on the website
 var colors = ['#4285f4', "#0f9d58", "#f4b400", "#db4437"]
+//var colors = ['#C0B283', '#DCD0C0', '#F4F4F4', '#373737']
 
 //Data model for the content of the website
-var content = [about, education];
+var content = [about, education, projects];
 
 //React Component to render the content of the website
 var ContentComponent = React.createClass({
   render: function () {
-    var navStyle = {
-      color: "black",
-      border: 0
-    }
     return (
       <div>
-        <ul style={ navStyle } className="nav nav-tabs">
+        <ul className="nav nav-tabs navbar-fixed-top">
           {this.props.data.map(function (section, idx) {
-            //Create tab style
-            var tabStyle = {
-              color: "black",
-              background: this.props.colors[idx % 4],
-              border: 0
-            };
             return (
-              <li style={ tabStyle }><a href="#">{ section.title }</a></li>
+              <li><a id={"color-" + (idx % 4 + 1)} href="#">{ section.title }</a></li>
             )
           }, this)}
         </ul>
         {this.props.data.map(function (section, idx) {
-          //Create the div style
-          //console.log(this.props.colors);
-          var divStyle = {
-            background: this.props.colors[idx % 4],
-            margin: 0,
-            border: 0
-          };
-          //Create the text style
-          var textStyle = {
-            color: 'black',
-            margin: 10
-            //textShadow: '-1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff, 1px -1px 0 #fff'
-          };
-          //Create the image style
-          var imageStyle = {
-            display: 'block',
-            margin: 'auto'
-          };
           return (
-            <div style={ divStyle } className="well">
-              <h1 style={ textStyle }>{ section.title }</h1>
-              <img style={ imageStyle } src={ section.image } className="img-circle img-fluid" />
-              <p className="lead" style={ textStyle }>{ section.description }</p>
+            <div className="container-fluid" id={"color-" + (idx % 4 + 1)}>
+              <h2>{ section.title }</h2>
+              <img src={ section.image } className="img-circle img-fluid" />
+              <p className="lead">{ section.description }</p>
             </div>
           )
         }, this)}
